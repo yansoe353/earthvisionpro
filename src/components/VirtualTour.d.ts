2eb1e6ab3a95b969c5abdc1925c17adaefae3bcb
yansoe353 <yannaingsoe.itech@gmail.com>
2025-01-10 01:17:02 +0700

    Update VirtualTour.d.ts

--- src/components/VirtualTour.d.ts
+++ src/components/VirtualTour.d.ts
@@ -1,1 +1,10 @@
-P
+declare module './components/VirtualTour' {
+  import { FC } from 'react';
+
+  interface VirtualTourProps {
+    location: { lat: number; lng: number; name: string };
+  }
+
+  const VirtualTour: FC<VirtualTourProps>;
+  export default VirtualTour;
+}
