ec166b1b8dac9306b516b2496529c990fd03de38
yansoe353 <yannaingsoe.itech@gmail.com>
2025-01-10 12:06:52 +0700

    Update VirtualTour.tsx

--- src/components/VirtualTour.tsx
+++ src/components/VirtualTour.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { fetchMapillaryImage } from '../services/VirtualTourService';
-import './VirtualTour.css';
+import './index.css';
 
 interface Location {
   lat: number;
