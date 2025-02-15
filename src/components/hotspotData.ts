aa123a0b9487255eaa2959d68df2161f0c6f8293
yansoe353 <yannaingsoe.itech@gmail.com>
2025-02-15 17:29:22 +0700

    Create hotspotData.ts

---
+++ src/components/hotspotData.ts
@@ -0,0 +1,32 @@
+// hotspotData.ts
+export type Hotspot = {
+  id: string;
+  name: string;
+  description: string;
+  coordinates: [number, number];
+  iframeUrl: string;
+};
+
+export const hotspots: Hotspot[] = [
+  {
+    id: '1',
+    name: 'Central Park',
+    description: 'A large public park in New York City.',
+    coordinates: [-73.9654, 40.7829],
+    iframeUrl: 'https://captures-three.vercel.app/',
+  },
+  {
+    id: '2',
+    name: 'Eiffel Tower',
+    description: 'A famous landmark in Paris, France.',
+    coordinates: [2.2945, 48.8584],
+    iframeUrl: 'https://captures-three.vercel.app/',
+  },
+  {
+    id: '3',
+    name: 'Arosa Hörnli - Switzerland',
+    description: 'A famous Arosa Hörnli in Switzerland.',
+    coordinates: [9.714189134324783, 46.52136798216034],
+    iframeUrl: 'https://captures-three.vercel.app/swis.html',
+  },
+];
