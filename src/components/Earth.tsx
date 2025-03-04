717c12d5f0b25409ff1d7f690038975860861b7c
yansoe353 <yannaingsoe.itech@gmail.com>
2025-03-05 01:17:28 +0700

    Update Earth.tsx

--- src/components/Earth.tsx
+++ src/components/Earth.tsx
@@ -721,7 +721,7 @@ const Earth = forwardRef<EarthRef, EarthProps>(({ onCaptureView, showWeatherWidg
     }}
   >
     <div className="creature-marker">
-      <img src="https://i.ibb.co/j9HDFHJw/portal.png" alt="Custom Icon" style={{ width: '24px', height: '24px' }} />
+      <img src="https://i.ibb.co/dw8FdD4j/ppt.webp" alt="Custom Icon" style={{ width: '24px', height: '24px' }} />
     </div>
   </Marker>
 ))}
