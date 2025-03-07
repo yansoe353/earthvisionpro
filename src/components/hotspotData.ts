68a8ef150a5fc60f4cab71bb0e05e653589f815e
yansoe353 <yannaingsoe.itech@gmail.com>
2025-03-08 02:15:02 +0700

    Update hotspotData.ts

--- src/components/hotspotData.ts
+++ src/components/hotspotData.ts
@@ -1,14 +1,14 @@
 export const defaultHotspotData = [
   // your hotspot data here
   {
-    id: 'hotspot-1',
+    id: 'hotspot-1S',
     name: 'Hotspot 1',
     description: 'Description of Hotspot 1',
     coordinates: [100.0, 13.0],
     iframeUrl: 'https://example.com/hotspot1',
   },
   {
-    id: 'hotspot-2',
+    id: 'hotspot-2s',
     name: 'Hotspot 2',
     description: 'Description of Hotspot 2',
     coordinates: [101.0, 14.0],
