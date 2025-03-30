506d80e36596fcf96c6c78e695c8a877ff11f09e
yansoe353 <yannaingsoe.itech@gmail.com>
2025-03-30 09:39:26 +0700

    Update hotspotData.ts

--- src/components/hotspotData.ts
+++ src/components/hotspotData.ts
@@ -4,7 +4,7 @@ export const defaultHotspotData = [
     name: 'Corsewall Lighthouse Hotel and Restaurant',
     description: 'In a 19th-century working lighthouse, this quirky hotel and restaurant is 18 km from Stranraer and 26 km from Dunskey Golf Club in Portpatrick.',
     coordinates: [55.006647, -5.159099] as [number, number],
-    iframeUrl: 'https://example.com/hotspot1',
+    iframeUrl: 'https://captures-three.vercel.app/lighthouse.html',
   },
   {
     id: 'hotspot-2',
