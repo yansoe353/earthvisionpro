7fc0976830db313a7ce8565d236ee1522f9b53a1
yansoe353 <yannaingsoe.itech@gmail.com>
2025-03-30 09:38:22 +0700

    Update hotspotData.ts

--- src/components/hotspotData.ts
+++ src/components/hotspotData.ts
@@ -1,16 +1,16 @@
 export const defaultHotspotData = [
   {
     id: 'hotspot-1',
-    name: 'Hotspot 1',
-    description: 'Description of Hotspot 1',
-    coordinates: [100.0, 13.0] as [number, number],
+    name: 'Corsewall Lighthouse Hotel and Restaurant',
+    description: 'In a 19th-century working lighthouse, this quirky hotel and restaurant is 18 km from Stranraer and 26 km from Dunskey Golf Club in Portpatrick.',
+    coordinates: [55.006647, -5.159099] as [number, number],
     iframeUrl: 'https://example.com/hotspot1',
   },
   {
     id: 'hotspot-2',
     name: 'Hotspot 2',
     description: 'Description of Hotspot 2',
-    coordinates: [101.0, 14.0] as [number, number],
+    coordinates: [55.006647,-6.159099] as [number, number],
     iframeUrl: 'https://example.com/hotspot2',
   },
   // Add more hotspots as needed
