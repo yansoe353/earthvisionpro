7c98a9d75b7d3668a96660ce75387036673ea8b6
yansoe353 <yannaingsoe.itech@gmail.com>
2025-03-31 11:51:13 +0700

    Update disasterPOIs.ts

--- src/components/disasterPOIs.ts
+++ src/components/disasterPOIs.ts
@@ -24,7 +24,7 @@ const disasterPOIs: DisasterPOI[] = [
     image: 'https://earthvision.world/wp-content/uploads/2025/03/487205691_1161177069136995_3227935157759261818_n.jpg',
     link: 'https://earthvision.world/events/devastating-7-7-magnitude-earthquake-strikes-myanmar-over-1700-dead-as-rescue-efforts-continue/',
     helpLink: 'https://web.facebook.com/info.mrcs?_rdc=1&_rdr#',
-    supportLink: 'ttps://earthvision.world/mmearthquakesupport',
+    supportLink: 'https://earthvision.world/mmearthquakesupport',
     coordinates: [95.923703, 21.995095],
     effectZones: {
       epicenter: [95.923703, 21.995095],
